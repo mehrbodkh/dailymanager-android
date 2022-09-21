a1a1e19fb8fdc3eb3b12c164a4241706c94af3d7
Mehrbod <mehrbod@ridedott.com>
2022-09-22 00:13:22 +0200

    feat: add simple splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.3.0-beta02")
     implementation("androidx.compose.ui:ui-tooling-preview:1.3.0-beta02")
     implementation("androidx.compose.material:material:1.3.0-beta02")
+    implementation("androidx.core:core-splashscreen:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
